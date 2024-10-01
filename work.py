211dce643c967972b60222d87b8d8cfe917833db
twperlv <timothy-william.picard@edu.rtu.lv>
2024-10-01 13:28:33 +0300

    Second commit

--- work.py
+++ work.py
@@ -14,11 +14,19 @@ public_transport = Symbol("public transport")
 
 
 knowledge_base = And(
+    # If it's raining or there’s an early meeting, you should work from home
     Implication((Or(rain, early_meeting)), wfh),
+
+    # If it’s not raining and there’s no heavy traffic, you should drive
     Implication(And(Not(heavy_traffic), Not(rain)), drive),
+
+    # If there’s no strike and it’s not raining, you should take public transport
     Implication(And(Not(strike), Not(rain)), public_transport),
 
+    # If there is an appointment, you should drive
     Implication(appointment, drive),
+    
+    # If you overslept and there is any problem on the way to work, you should work at home
     Implication(And(overslept, Or(heavy_traffic, road_construction, rain), strike), wfh)
 )
 
@@ -26,9 +34,15 @@ print(f"\nKnowledge base: {knowledge_base.formula()}")
 
 
 def query(scenario, action):
+    '''
+    Defines a query given a specific scenario and the action to be considered. 
+    '''
     return Implication(scenario, action)
 
 def suggestions(scenario):
+    '''
+    Prints out the output for each possible action in the given scenario.
+    '''
     print(f"\nScenario: {scenario.formula()}")
     print(f"Work from home: {model_check(knowledge_base, query(scenario, wfh))}")
     print(f"Take public transport: {model_check(knowledge_base, query(scenario, public_transport))}")
